efb0d473ce3c5555f6ba315579ec832f10cdfdf8
Kyle Cogswell <kyle.cogswell@defence.redspace.com>
2026-06-01 19:28:25 -0300

    chore: adding jott

--- Formula/jott.rb
+++ Formula/jott.rb
@@ -1,7 +1,7 @@
 class Jott < Formula
   desc "Minimal, terminal-centric markdown time tracker"
   homepage "https://github.com/k-cogswell/jott"
-  url "https://github.com/k-cogswell/jott/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/k-cogswell/jott/archive/refs/tags/1.0.0.tar.gz"
   sha256 "96725f58217cb50a1d283f8afe94b2830a4b46aba1a1ca6ab0fcecfb042a1e98"
   license "MIT"
 
