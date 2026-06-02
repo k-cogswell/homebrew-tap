be713f9b2d77df40bac293e96f219f8f01fbbb53
Kyle Cogswell <kcogswell26@gmail.com>
2026-06-02 08:50:22 -0300

    jott 1.0.1

--- Formula/jott.rb
+++ Formula/jott.rb
@@ -1,8 +1,8 @@
 class Jott < Formula
   desc "Minimal, terminal-centric markdown time tracker"
   homepage "https://github.com/k-cogswell/jott"
-  url "https://github.com/k-cogswell/jott/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "96725f58217cb50a1d283f8afe94b2830a4b46aba1a1ca6ab0fcecfb042a1e98"
+  url "https://github.com/k-cogswell/jott/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "6127ad4546a34b595fee0266d27b8ff7306f00313a9b70fe26e86b7f431619e5"
   license "MIT"
 
   def install
